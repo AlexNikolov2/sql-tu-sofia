5e3ae2bfc0bc3a8bd981044d72ef5ee94e6c428f
Aleks Nikolov <tcr.official.world@gmail.com>
2025-02-17 12:03:41 +0200

    update again

--- ex4_2.sql
+++ ex4_2.sql
@@ -4,6 +4,6 @@ create table students(id int auto_increment primary key, name varchar(100) uniqu
 
 create table coaches(id int auto_increment primary key, name varchar(100) unique, sport varchar(100), foreign key (sport) references sport_clubs(club) on delete cascade);
 
-create table football(id int auto_increment primary key, student_id int, coach_id int, foreign key (student_id) references students(id) on delete cascade, foreign key (id) references coaches(id) on delete cascade);
+create table football(id int auto_increment primary key, student_id int, coach_id int, foreign key (student_id) references students(id) on delete cascade, foreign key (coach_id) references coaches(id) on delete cascade);
 
-create table volleyball(id int auto_increment primary key, student_id int, coach_id int, foreign key (student_id) references students(id) on delete cascade, foreign key (id) references coaches(id) on delete cascade);
+create table volleyball(id int auto_increment primary key, student_id int, coach_id int, foreign key (student_id) references students(id) on delete cascade, foreign key (coach_id) references coaches(id) on delete cascade);
