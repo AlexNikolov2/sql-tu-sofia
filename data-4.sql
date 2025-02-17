f1be2e2b4f65a9925aecb1daf43d79c35b2c1163
Aleks Nikolov <tcr.official.world@gmail.com>
2025-02-17 12:04:09 +0200

    .

---
+++ data-4.sql
@@ -0,0 +1,22 @@
+-- Insert Sport Clubs
+INSERT INTO sport_clubs (club) VALUES 
+('Football'), 
+('Volleyball');
+
+-- Insert Students
+INSERT INTO students (name, class, age, sport) VALUES 
+('Ivan Petrov', 10, 16, 'Football'),
+('Maria Georgieva', 9, 15, 'Volleyball'),
+('Georgi Dimitrov', 11, 17, 'Football'),
+('Elena Kostova', 8, 14, 'Volleyball'),
+('Nikolay Stoyanov', 12, 18, 'Football'),
+('Hristo Kolev', 10, 16, 'Volleyball'),
+('Desislava Ivanova', 9, 15, 'Football'),
+('Todor Yordanov', 11, 17, 'Volleyball'),
+('Svetlana Borisova', 8, 14, 'Football'),
+('Emil Radev', 12, 18, 'Volleyball');
+
+-- Insert Coaches
+INSERT INTO coaches (name, sport) VALUES 
+('Coach Stefan', 'Football'),
+('Coach Dimitrov', 'Volleyball');
