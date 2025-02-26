f37efe4b15c923c378c7405d1ef6dae0195a71ab
Aleks Nikolov <tcr.official.world@gmail.com>
2025-02-26 09:07:06 +0200

    .

---
+++ lab-2/ex7_1.sql
@@ -0,0 +1,1 @@
+create table users (id int primary key auto_increment, username varchar (100) not null unique, email varchar(100) not null unique, password_hash varchar(255) not null, is_admin boolean default false, is_banned boolean default false, created_at timestamp default current_timestamp)
