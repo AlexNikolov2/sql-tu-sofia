ca92c912e5182e5a9a40a9329b289809c3e17a3e
Aleks Nikolov <tcr.official.world@gmail.com>
2025-02-26 08:33:28 +0200

    .

---
+++ lab-2/ex2.sql
@@ -0,0 +1,3 @@
+create table coaches (id int not null unique auto_increment, name varchar(100) not null, egn varchar(10));
+
+insert into coaches (name, egn) value ('Gosho Goshov', '1234567890'), ('Pesho Peshov', '6992681012'), ('Ganyo Ganyov', '0987654321')
