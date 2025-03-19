05015e819ababe6ec27cc0fa23114df81d344543
Aleks Nikolov <tcr.official.world@gmail.com>
2025-03-19 09:30:23 +0200

    .

--- lab-5/ex4.sql
+++ lab-5/ex4.sql
@@ -1,18 +1,7 @@
-SELECT 
-    s1.name AS student_1, 
-    s2.name AS student_2, 
-    sg.id AS sportgroup_id
-FROM 
-    student_sport ss1
-JOIN 
-    student_sport ss2 ON ss1.sportgroup_id = ss2.sportgroup_id AND ss1.student_id < ss2.student_id
-JOIN 
-    students s1 ON ss1.student_id = s1.id
-JOIN 
-    students s2 ON ss2.student_id = s2.id
-JOIN 
-    sportgroups sg ON ss1.sportgroup_id = sg.id
-JOIN 
-    sports sp ON sg.sport_id = sp.id
-WHERE 
-    sp.name = 'Football';
+select s1.name as student_1, s2.name as student_2, sg.id as sportgroup_id from student_sport ss1
+join student_sport ss2 on ss1.sportgroup_id = ss2.sportgroup_id AND ss1.student_id < ss2.student_id
+join students s1 on ss1.student_id = s1.id
+join students s2 on ss2.student_id = s2.id
+join sportgroups sg on ss1.sportgroup_id = sg.id
+join sports sp on sg.sport_id = sp.id
+where sp.name = 'Football';
