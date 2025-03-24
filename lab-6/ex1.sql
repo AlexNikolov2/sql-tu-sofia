a25dcf3005e4647259599a953f58124bc170ca13
Aleks Nikolov <tcr.official.world@gmail.com>
2025-03-24 10:50:43 +0200

    .

--- lab-6/ex1.sql
+++ lab-6/ex1.sql
@@ -1,2 +1,6 @@
-update coaches 
-set id = 32 where id = 1
+alter table sportgroups 
+drop foreign key sportgroups_ibfk_2;
+
+alter table sportgroups 
+add constraint foreign key `coach_id_key` (coach_id) references coaches(id)
+on delete set null on update cascade
