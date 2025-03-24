ddd984d81e5ea3b80813dfd7d718b952be8a1aab
Aleks Nikolov <tcr.official.world@gmail.com>
2025-03-24 11:08:51 +0200

    .

---
+++ lab-6/ex7_1.sql
@@ -0,0 +1,7 @@
+begin;
+use school_sport_clubs;
+select students.name, class, phone from students 
+join student_sport on students.id = student_sport.student_id
+join sports on sports.id = student_sport.sportgroup_id
+where sports.name = 'Football';
+commit;
