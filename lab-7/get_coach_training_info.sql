2ac9b2be944d8b7191fc3489f591d0591fc381c9
Aleks Nikolov <tcr.official.world@gmail.com>
2025-03-31 11:46:35 +0300

    .

---
+++ lab-7/get_coach_training_info.sql
@@ -0,0 +1,22 @@
+USE school_sport_clubs;
+DROP PROCEDURE IF EXISTS getCoachTrainingInfo;
+DELIMITER |
+CREATE PROCEDURE getCoachTrainingInfo(IN coachName VARCHAR(255))
+BEGIN
+    SELECT 
+        sp.name AS sportName,
+        sg.location AS trainingLocation,
+        sg.hourOfTraining AS trainingHour,
+        sg.dayOfWeek AS trainingDay,
+        st.name AS studentName,
+        st.phone AS studentPhone
+    FROM sportgroups sg
+    JOIN sports sp ON sg.sport_id = sp.id
+    JOIN student_sport ss ON sg.id = ss.sportgroup_id
+    JOIN students st ON ss.student_id = st.id
+    JOIN coaches c ON sg.coach_id = c.id
+    WHERE c.name = coachName;
+END |
+DELIMITER ;
+-- Example query to test the procedure
+CALL getCoachTrainingInfo('Slavi Petkov Petkov');
