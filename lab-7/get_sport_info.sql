7a1683d50ee7a43936d782d77cfe5d6b49fa3143
Aleks Nikolov <tcr.official.world@gmail.com>
2025-03-31 11:49:09 +0300

    .

---
+++ lab-7/get_sport_info.sql
@@ -0,0 +1,19 @@
+USE school_sport_clubs;
+DROP PROCEDURE IF EXISTS getSportInfo;
+DELIMITER |
+CREATE PROCEDURE getSportInfo(IN sportId INT)
+BEGIN
+    SELECT 
+        sp.name AS sportName,
+        st.name AS studentName,
+        c.name AS coachName
+    FROM sports sp
+    JOIN sportgroups sg ON sp.id = sg.sport_id
+    JOIN student_sport ss ON sg.id = ss.sportgroup_id
+    JOIN students st ON ss.student_id = st.id
+    JOIN coaches c ON sg.coach_id = c.id
+    WHERE sp.id = sportId;
+END |
+DELIMITER ;
+-- Example query to test the procedure
+-- CALL getSportInfo(1);
