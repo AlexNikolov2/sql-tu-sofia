fd5c41b34d37f6f3d1b820a22bfb0bd371c74f76
Aleks Nikolov <tcr.official.world@gmail.com>
2025-04-06 14:49:37 +0300

    .

---
+++ course-project/ex8_make_new_commit.sql
@@ -0,0 +1,14 @@
+CREATE TABLE CommitLog (
+    log_id INT AUTO_INCREMENT PRIMARY KEY,
+    commit_id INT,
+    log_time DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+DELIMITER //
+CREATE TRIGGER log_commit AFTER INSERT ON Commit
+FOR EACH ROW
+BEGIN
+    INSERT INTO CommitLog (commit_id) VALUES (NEW.commit_id);
+END;
+//
+DELIMITER ;
