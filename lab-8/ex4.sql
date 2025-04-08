607951ef4afc8b9debab38fd53abd805093bbce1
Aleks Nikolov <tcr.official.world@gmail.com>
2025-04-09 02:54:55 +0300

    .

---
+++ lab-8/ex4.sql
@@ -0,0 +1,32 @@
+USE school_sport_clubs;
+
+DELIMITER |
+CREATE PROCEDURE ListStudentsAndGroupCount()
+BEGIN
+    DECLARE done INT DEFAULT 0;
+    DECLARE studentName VARCHAR(255);
+    DECLARE studentId INT;
+    DECLARE groupCount INT;
+    DECLARE cur CURSOR FOR SELECT id, name FROM students;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+
+    OPEN cur;
+
+    read_loop: LOOP
+        FETCH cur INTO studentId, studentName;
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+
+        SELECT COUNT(*) INTO groupCount
+        FROM student_sport
+        WHERE student_id = studentId;
+
+        SELECT CONCAT('Student: ', studentName, ' | Groups Participated: ', groupCount) AS Result;
+    END LOOP;
+
+    CLOSE cur;
+END |
+DELIMITER ;
+
+CALL ListStudentsAndGroupCount();
