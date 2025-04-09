8b894e2eedd51e78fa1eb4464f569c0d86273f6b
Aleks Nikolov <tcr.official.world@gmail.com>
2025-04-09 09:03:32 +0300

    .

--- lab-8/ex5.sql
+++ lab-8/ex5.sql
@@ -1,4 +1,9 @@
+use transaction_test;
+
+DROP PROCEDURE IF EXISTS transfer_money_with_conversion;
+
 DELIMITER //
+
 CREATE PROCEDURE transfer_money_with_conversion(
 IN from_account_id INT,
 IN to_account_id INT,
@@ -14,21 +19,21 @@ START TRANSACTION;
 -- Проверка на валутите
 SELECT currency, amount INTO from_currency, current_balance
 FROM customer_accounts WHERE id = from_account_id FOR UPDATE;
-IF current_balance &lt; transfer_amount THEN
-SIGNAL SQLSTATE &#39;45000&#39; SET MESSAGE_TEXT = &#39;Insufficient funds&#39;;
+IF current_balance < transfer_amount THEN
+SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Insufficient funds';
 END IF;
 SELECT currency INTO to_currency
 FROM customer_accounts WHERE id = to_account_id FOR UPDATE;
 -- Конвертиране
 IF from_currency = to_currency THEN
 SET converted_amount = transfer_amount;
-ELSEIF from_currency = &#39;BGN&#39; AND to_currency = &#39;EUR&#39; THEN
+ELSEIF from_currency = 'BGN' AND to_currency = 'EUR' THEN
 SET converted_amount = transfer_amount / exchange_rate;
-ELSEIF from_currency = &#39;EUR&#39; AND to_currency = &#39;BGN&#39; THEN
+ELSEIF from_currency = 'EUR' AND to_currency = 'BGN' THEN
 SET converted_amount = transfer_amount * exchange_rate;
 ELSE
-SIGNAL SQLSTATE &#39;45000&#39; SET MESSAGE_TEXT = &#39;Unsupported currency
-conversion&#39;;
+SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Unsupported currency
+conversion';
 END IF;
 -- Актуализиране на балансите
 UPDATE customer_accounts
@@ -40,5 +45,7 @@ SET amount = amount + converted_amount
 WHERE id = to_account_id;
 COMMIT;
 END //
-DELIMITER ;
-CALL transfer_money_with_conversion(1, 2, 1000, 1.95583);
+
+DELIMITER;
+
+CALL transfer_money_with_conversion (1, 2, 1000, 1.95583);
