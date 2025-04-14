22e838aee74281e83ecfda27a37f104867bce8bf
Aleks Nikolov <tcr.official.world@gmail.com>
2025-04-14 22:06:40 +0300

    .

--- course-project/ex2_select_table.sql
+++ course-project/ex2_select_table.sql
@@ -1,5 +1,7 @@
 --CHECK COMMITS BY A GIVEN USER ID
 
 use git_projekt_system;
-SELECT * FROM Commit
-WHERE developer_id = 2 AND commit_time > '2023-01-01';
+SELECT d.name, c.*
+FROM Commit c
+JOIN Developer d ON c.developer_id = d.developer_id
+WHERE c.developer_id = 2 AND c.commit_time > '2023-01-01';
--- course-project/ex3_count_commits.sql
+++ course-project/ex3_count_commits.sql
@@ -1,3 +1,5 @@
+--Count each developer's total number of commits
+
 SELECT developer_id, COUNT(*) AS total_commits
 FROM Commit
-GROUP BY developer_id;
+GROUP BY developer_id
