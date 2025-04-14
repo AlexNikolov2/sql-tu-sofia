2031fd4dd6a87a154947025ee7bf3da276a18b9a
Aleks Nikolov <tcr.official.world@gmail.com>
2025-04-15 00:53:09 +0300

    .

--- course-project/ex5_left_join.sql
+++ course-project/ex5_left_join.sql
@@ -1,5 +1,5 @@
--- същото, но с outer join - всички разработчици, дори и без комити
+-- същото, но с left join - всички разработчици, дори и без комити
 
 SELECT d.name, c.message
 FROM Developer d
-OUTER JOIN Commit c ON d.developer_id = c.developer_id;
+LEFT JOIN Commit c ON d.developer_id = c.developer_id;
