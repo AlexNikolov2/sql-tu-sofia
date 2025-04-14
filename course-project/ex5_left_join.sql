4218da12707ca6c6c7cee7a2899d8c27331a4395
Aleks Nikolov <tcr.official.world@gmail.com>
2025-04-14 23:17:18 +0300

    .

--- course-project/ex5_left_join.sql
+++ course-project/ex5_left_join.sql
@@ -1,5 +1,5 @@
--- същото, но с left join - таблицата Developer е лява и има приоритет при взимането на данни
+-- същото, но с outer join - всички разработчици, дори и без комити
 
 SELECT d.name, c.message
 FROM Developer d
-LEFT JOIN Commit c ON d.developer_id = c.developer_id;
+OUTER JOIN Commit c ON d.developer_id = c.developer_id;
