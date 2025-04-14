9a6f99d5b9d793ba54d91d01942623533c6db5d2
Aleks Nikolov <tcr.official.world@gmail.com>
2025-04-14 22:19:06 +0300

    .

--- course-project/ex7_aggregate_function.sql
+++ course-project/ex7_aggregate_function.sql
@@ -1,6 +1,7 @@
---show all commits for each project
+--Брой на къмити по проект
 
 SELECT p.name, COUNT(c.commit_id) AS total_commits
 FROM Project p
 JOIN Commit c ON p.project_id = c.project_id
-GROUP BY p.project_id;
+GROUP BY p.project_id, p.name
+ORDER BY total_commits DESC;
