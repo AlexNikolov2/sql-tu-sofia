54e551bf9ae0863ccf9842013ee6749d78753e94
Aleks Nikolov <tcr.official.world@gmail.com>
2025-04-14 22:08:36 +0300

    .

--- course-project/ex3_count_commits.sql
+++ course-project/ex3_count_commits.sql
@@ -1,5 +1,6 @@
 --Count each developer's total number of commits
 
-SELECT developer_id, COUNT(*) AS total_commits
+SELECT name, COUNT(*) AS total_commits
 FROM Commit
-GROUP BY developer_id
+JOIN Developer ON Commit.developer_id = Developer.developer_id
+GROUP BY Developer.developer_id
