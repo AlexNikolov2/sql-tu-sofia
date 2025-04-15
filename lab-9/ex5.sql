79b67922cfa608a433af1a911672117f24a31c06
Aleks Nikolov <tcr.official.world@gmail.com>
2025-04-16 01:29:20 +0300

    .

---
+++ lab-9/ex5.sql
@@ -0,0 +1,16 @@
+CREATE PROCEDURE get_coaches_by_sport(IN sport_name VARCHAR(255))
+BEGIN
+    SELECT 
+        c.name AS coach_name,
+        sg.location AS training_location,
+        sg.hourOfTraining AS training_time,
+        sg.dayOfWeek AS training_day
+    FROM 
+        coaches c
+    JOIN 
+        sportGroups sg ON c.id = sg.coach_id
+    JOIN 
+        sports sp ON sg.sport_id = sp.id
+    WHERE 
+        sp.name = sport_name;
+END;
