f31b85195c440311370f4a6975833b837029e562
Aleks Nikolov <tcr.official.world@gmail.com>
2025-04-16 08:20:16 +0300

    .

--- lab-9/ex1.sql
+++ lab-9/ex1.sql
@@ -1,7 +1,11 @@
+DELIMITER $$
+
 CREATE TRIGGER trg_salarypayments_delete
 AFTER DELETE ON salaryPayments
 FOR EACH ROW
 BEGIN
     INSERT INTO salaryPayments_log (id, coach_id, month, year, salaryAmount, dateOfPayment)
     VALUES (OLD.id, OLD.coach_id, OLD.month, OLD.year, OLD.salaryAmount, OLD.dateOfPayment);
-END;
+END$$
+
+DELIMITER ;
